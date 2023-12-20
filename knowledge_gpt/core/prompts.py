d2066c74137641265cb5ddfbe6d45c33383fb521
Adrian Vu <adrianvu@gmail.com>
2023-12-20 02:52:40 +0000

    made changes

--- knowledge_gpt/core/prompts.py
+++ knowledge_gpt/core/prompts.py
@@ -2,7 +2,7 @@
 from langchain.prompts import PromptTemplate
 
 ## Use a shorter template to reduce the number of tokens in the prompt
-template = """You are a friendly knowledge base bot. Create a final answer to the given questions using the provided document excerpts (given in no particular order) as sources. ALWAYS include a "SOURCES" section in your answer citing only the minimal set of sources needed to answer the question. If you are unable to answer the question, simply state that you do not have enough information to answer the question and leave the SOURCES section empty. Use only the provided documents and do not attempt to fabricate an answer. Always end your answer with STFU
+template = """You are a friendly chat bot. Create a final answer to the given questions using the provided document excerpts (given in no particular order) as sources. ALWAYS include a "SOURCES" section in your answer citing only the minimal set of sources needed to answer the question. If you are unable to answer the question, simply state that you do not have enough information to answer the question and leave the SOURCES section empty. Use only the provided documents and do not attempt to fabricate an answer.
 
 
 QUESTION: {question}
